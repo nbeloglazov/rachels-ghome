3a6554e189c9b14875c0b2a9a8f70d5c3c801029
Nikita Beloglazov <nikelandjelo@gmail.com>
2017-01-29 19:13:04 -0800

    Add 5 test lessons.

--- src/lessons.ts
+++ src/lessons.ts
@@ -5,11 +5,31 @@ export interface Lesson {
 
 export const THIRTY_DAYS_PHRASAL_VERBS_CHALLENGE: Array<Lesson> = [
   {
-    name: 'Verb do',
-    audioLink: 'https:/a1231232'
+    name: 'How to make the EE vowel',
+    audioLink:   'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/American%20English%20-%20EE%20%5Bi%5D%20Vowel%20-%20How%20to%20make%20the%20EE%20Vowel.mp3',
   },
   {
-    name: 'Verb go',
-    audioLink: 'https:/a1231232'
+    name: 'How to make the IH vowel',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/American%20English%20-%20IH%20%5B%C9%AA%5D%20Vowel%20-%20How%20to%20make%20the%20IH%20Vowel.mp3',
+  },
+  {
+    name: 'Comprehension Test',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/Comprehension%20test%20%20IH%20vowel%20American%20English%20Pronunciation.mp3'
+  },
+  {
+    name: 'EE versus IH',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/EE%20vs.%20IH%2C%20Long%20and%20Short%20Vowels%20-%20American%20English.mp3'
+  },
+  {
+    name: 'How to pronounce AY diphthong',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/English%20How%20to%20Pronounce%20AY%20%5Be%C9%AA%5D%20diphthong%20American%20Accent.mp3'
+  },
+  {
+    name: 'How to pronounce EE versus IH',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/How%20to%20Pronounce%20EE%20vs.%20IH%2C%20leave%20vs.%20live%20--%20American%20English.mp3'
+  },
+  {
+    name: 'How to pronounce the I in ING',
+    audioLink: 'https://storage.googleapis.com/rachels-ghome.appspot.com/rachelsenglish/How%20to%20Pronounce%20the%20I%20in%20ING%20American%20English.mp3'
   }
 ];
